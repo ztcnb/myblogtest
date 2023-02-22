e4f81c0131e17bbb6cc565371cb835e66325e99e
ztc <2283813342@qq.com>
2023-02-22 17:03:02 +0800

    发布测试1

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,3 +1,16 @@
+
 module.exports = {
+    // base: "/myblogtest/",
+    themeConfig: {
+        // lastUpdated: '更新时间',
+        // logo: '/assets/img/logo.png',
+        //  导航栏链接
+        nav: [
+            {text: 'Home', link: '/'},
+            {text: 'Guide', link: '/guide/'},
+            {text: '面试指南', link: '/mszn/'},
+            {text: 'External', link: 'https://google.com'},
+        ]
+    }
 
 }
